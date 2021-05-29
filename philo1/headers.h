c7fb49063e44bf563d6e68bb223181a0e02764b6
soukainahikma <soukainahikma@gmail.com>
2021-05-29 17:01:17 +0100

    testing thread join instead of detach

---
+++ philo1/headers.h
@@ -0,0 +1,46 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<limits.h>
+#include<pthread.h>
+#include<semaphore.h>
+#include <sys/time.h>
+
+typedef struct s_philo
+{
+	int				number_of_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				nb_philo_must_eat;
+	int				i;
+	int				a;
+	pthread_t		*tid;
+	pthread_mutex_t	*lock;
+	pthread_mutex_t	myfork;
+	pthread_mutex_t	died;
+	pthread_mutex_t	life;
+
+	struct timeval	start_eating;
+	struct timeval	init;
+	long			duration;
+
+}				t_philo;
+int n_g;
+pthread_mutex_t	test_g;
+pthread_mutex_t	g_print;
+
+int		ft_atoi(const char *str);
+void	printer(t_philo *philo);
+void	table(t_philo *philo);
+long get_duration(struct timeval start_tp);
+long	get_time_milisecond(long sec, long usec);
+void	lock_forks1(t_philo *philo,int i);
+void	lock_forks2(t_philo *philo,int i);
+
+
+#endif
---
+++ philo1/sources/table.c
@@ -0,0 +1,141 @@
+#include "../headers.h"
+void	eat(t_philo *philo)
+{
+	struct timeval tp;
+	pthread_mutex_lock(&philo->lock[philo->i]);
+	lock_forks1(philo, philo->i);
+	pthread_mutex_lock(&philo->lock[(philo->i + 1) % philo->number_of_philo]);
+	lock_forks2(philo, philo->i);
+	gettimeofday(&tp, NULL);
+	philo->start_eating = tp;
+	pthread_mutex_lock(&g_print);
+	philo->duration = get_duration(philo->init);
+	if(philo->nb_philo_must_eat != -2)
+		philo->nb_philo_must_eat--;
+	printf("%ld %d is eating\n", philo->duration , philo->i + 1);
+	pthread_mutex_unlock(&g_print);
+	usleep(philo->time_to_eat * 1000);
+	pthread_mutex_unlock(&philo->lock[(philo->i + 1) % philo->number_of_philo]);
+	pthread_mutex_unlock(&philo->lock[philo->i]);
+}
+
+void	think(t_philo *p)
+{
+	pthread_mutex_lock(&g_print);
+	p->duration = get_duration(p->init);
+	printf("%ld Philosopher %d is thinking\n", p->duration, p->i + 1);
+	pthread_mutex_unlock(&g_print);
+}
+
+void	sleep_(t_philo *p)
+{
+	pthread_mutex_lock(&g_print);
+	p->duration = get_duration(p->init);
+	printf("%ld %d is sleeping\n",p->duration, p->i + 1);
+	pthread_mutex_unlock(&g_print);
+	usleep(p->time_to_sleep * 1000);
+}
+void *death_check(void *philo)
+{
+	t_philo *p;
+	p = (t_philo *)philo;
+
+	while(1)
+	{
+		if(get_duration(p->start_eating) > p->time_to_die)
+		{
+			pthread_mutex_lock(&g_print);
+			if(p->nb_philo_must_eat > 0 || p->nb_philo_must_eat == -2)
+				printf("%ld %d died\n",get_duration(p->start_eating), p->i + 1);
+			pthread_mutex_unlock(&test_g);
+			break ;
+		}
+		usleep(1000);
+	}
+	return (NULL);
+}
+
+void	*philosopher(void *philo)
+{
+	t_philo	*test;
+	int		phil;
+	pthread_t death_checker;
+	struct timeval tp;
+
+	test = (t_philo *) philo;
+	gettimeofday(&tp , NULL);
+	test->start_eating = tp;
+	pthread_create(&death_checker, NULL,death_check, test);
+	usleep(100);
+	// pthread_detach(death_checker);
+	while (1)
+	{
+		if(test->nb_philo_must_eat > 0 || test->nb_philo_must_eat == -2)
+		{
+			eat(test);
+			sleep_(test);
+			think(test);
+		}
+	}
+	pthread_join(death_checker,NULL);
+	return (NULL);
+}
+
+t_philo *get_struc(t_philo *philo)
+{
+	t_philo *p;
+
+	p = (t_philo *)malloc(sizeof(t_philo));
+	p->number_of_philo = philo->number_of_philo;
+	p->time_to_die = philo->time_to_die;
+	p->time_to_eat = philo->time_to_eat;
+	p->time_to_sleep = philo->time_to_sleep;
+	p->nb_philo_must_eat = philo->nb_philo_must_eat;
+	p->i = philo->i;
+	p->a = philo->a;
+	p->tid = philo->tid;
+	p->lock = philo->lock;
+	p->myfork = philo->myfork;
+	p->start_eating = philo->start_eating;
+	p->init = philo->init;
+	p->duration = philo->duration;
+	p->life = philo->life;
+	return (p);
+}
+
+void	table(t_philo *phill)
+{
+	phill->i = 0;
+	// n_g = phill->
+	pthread_mutex_init(&test_g, NULL);
+	pthread_mutex_init(&g_print, NULL);
+	while (phill->i < phill->number_of_philo)
+	{
+		pthread_mutex_init(&phill->lock[phill->i], NULL);
+		phill->i++;
+	}
+	phill->i = 0;
+	pthread_mutex_lock(&test_g);
+	while (phill->i < phill->number_of_philo)
+	{
+		phill->a = phill->i;
+		pthread_create(&phill->tid[phill->i], NULL, philosopher, (void *)get_struc(phill));
+		usleep(100);
+		// pthread_detach(phill->tid[phill->i]);
+		phill->i++;
+	}
+	phill->i = 0;
+	while (phill->i < phill->number_of_philo)
+	{
+		pthread_join(phill->tid[phill->i],NULL);
+		phill->i++;
+	}
+	phill->i = 0;
+	while(phill->i < phill->number_of_philo)
+	{
+		pthread_mutex_destroy(&phill->lock[phill->i]);	
+		phill->i++;
+	}
+	pthread_mutex_lock(&test_g);
+	// pthread_mutex_unlock(&test_g);
+}
